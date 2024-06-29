dd7714f792ead3ab3543f3ae30f2e7a47e2a1809
joan.peruchet <joan17cast@gmail.com>
2024-06-29 13:00:13 +0200

    Setup podcast page with the episode table

--- src/pages/podcast.tsx
+++ src/pages/podcast.tsx
@@ -1,3 +1,5 @@
+import { Table } from "@/components/table";
+import { PodcastEpisodeModel } from "@/domain/podcast.domain";
 import { useGetPodcastById } from "@/persistence/podcast.persistence";
 import { useParams } from "@tanstack/react-router";
 import { match } from "ts-pattern";
@@ -7,14 +9,26 @@ function Podcast() {
     strict: true,
     from: undefined,
   });
-  console.log(podcastId);
   const getAllPodcast = useGetPodcastById(podcastId);
   return match(getAllPodcast)
     .with({ status: "pending" }, () => <div>Loading...</div>)
     .with({ status: "error" }, () => <div>Error</div>)
     .with({ status: "success" }, (fetchData) => {
-      console.log(fetchData);
-      return <>Podcast info here</>;
+      const listOfEpisodes: PodcastEpisodeModel[] =
+        fetchData.data.results.filter(
+          (e) => e.wrapperType === "podcastEpisode",
+        );
+      fetchData.data.results.filter((e) => e.wrapperType === "podcastEpisode");
+      return (
+        <>
+          <div className="flex flex-col gap-4 p-10">
+            <section className="w-full rounded-md px-4 py-2 shadow-md ring-1 ring-gray-200">
+              <h2 className="text-lg font-bold">{`Episodes: ${listOfEpisodes.length}`}</h2>
+            </section>
+            <Table listOfEpisodes={listOfEpisodes} />
+          </div>
+        </>
+      );
     })
     .exhaustive();
 }
