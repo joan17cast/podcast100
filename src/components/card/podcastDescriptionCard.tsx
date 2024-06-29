497f5fdf9fdbc5d2e1ad4ee7e48fba9b5f7ceeec
joan.peruchet <joan17cast@gmail.com>
2024-06-29 23:55:30 +0200

    Fix text alignment on podcast description card

--- src/components/card/podcastDescriptionCard.tsx
+++ src/components/card/podcastDescriptionCard.tsx
@@ -24,14 +24,14 @@ function PodcastDescriptionCard({ id }: PodcastDescriptionCardProps) {
               className="h-40 w-40 rounded-md object-cover"
             />
             <hr className="w-full rounded-md border border-gray-300" />
-            <div>
+            <div className="w-full">
               <h1 className="w-full truncate text-left font-semibold">
                 {podcast["im:name"].label}
               </h1>
               <p className="w-full truncate text-left font-light">{`by ${podcast["im:artist"].label}`}</p>
             </div>
             <hr className="w-full rounded-md border border-gray-300" />
-            <div>
+            <div className="w-full">
               <h1 className="w-full text-left">Description:</h1>
               <p className="w-full truncate text-wrap text-left font-light">
                 {podcast.summary.label}
