4b39e56abcf47c3cc2aef644bb69567adc1600fa
joan.peruchet <joan17cast@gmail.com>
2024-06-30 19:38:48 +0200

    Add link for podcast episodes

--- src/components/table/tableItem.tsx
+++ src/components/table/tableItem.tsx
@@ -1,15 +1,32 @@
 import { formatDate } from "@/utils/dateUtils";
+import { Link } from "@tanstack/react-router";
 
 interface TableItemProps {
   title: string;
   releaseDate: string;
   duration: number;
+  episodeId: number;
+  podcastId: number;
 }
 
-function TableItem({ title, releaseDate, duration }: TableItemProps) {
+function TableItem({
+  title,
+  releaseDate,
+  duration,
+  episodeId,
+  podcastId,
+}: TableItemProps) {
   return (
     <tr className="border-b-[1px]">
-      <th className="p-2 text-left font-normal">{title}</th>
+      <th className="p-2 text-left font-normal">
+        <Link
+          to="/podcast/$podcastId/episode/$episodeId"
+          params={{ podcastId: podcastId, episodeId: episodeId }}
+          className="text-blue-800 hover:text-blue-500"
+        >
+          {title}
+        </Link>
+      </th>
       <th className="font-normal">{formatDate("DD/MM/YYYY", releaseDate)}</th>
       <th className="font-normal">
         {formatDate("HH:mm:ss", duration.toString())}
