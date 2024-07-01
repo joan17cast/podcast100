86a7aa3f3a134542aa71790471170dcc938c138e
joan.peruchet <joan17cast@gmail.com>
2024-07-01 18:00:37 +0200

    Fix styles for description

--- src/components/card/podcastDescriptionCard.tsx
+++ src/components/card/podcastDescriptionCard.tsx
@@ -43,7 +43,7 @@ function PodcastDescriptionCard({ podcastId }: PodcastDescriptionCardProps) {
             </div>
             <hr className="w-full rounded-md border border-gray-300" />
             <div className="w-full">
-              <h1 className="font-semibold w-full text-left">Description:</h1>
+              <h1 className="w-full text-left font-semibold">Description:</h1>
               <p className="w-full truncate text-wrap text-left font-light">
                 {podcast.summary.label}
               </p>
